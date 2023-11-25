b381737ba8526d04de65e2e39079020fe3a6b4c2
dev.ankur <ankurbhati9027@gmail.com>
2023-11-25 08:37:10 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Destiny_home_page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag"]
+};
