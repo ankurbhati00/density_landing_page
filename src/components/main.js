162596fcb8ad79b13b412c1c4172db62aada029e
dev.ankur <ankurbhati9027@gmail.com>
2023-11-26 15:33:04 +0530

    project is ready

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import React from "react";
+// <-------All the styles className--->
 import {
   statisticsContainer,
   boxContainer,
@@ -12,6 +13,14 @@ import {
   overviewContainer,
   overviewBox,
   BSTDIMG,
+  startSmallContainer,
+  startSmallImagesContainer,
+  derivatesContainer,
+  derivatesUserGuideContainer,
+  guideBox,
+  iphone14ImgContainer,
+  guideInfoContainer,
+  backedByTheBestContainer,
 } from "../styles/main.module.css";
 import phone1 from "../assets/phone_single1.png";
 import phone2 from "../assets/phone_single2.png";
@@ -19,6 +28,15 @@ import phone3 from "../assets/phone_single3.png";
 import coinComparisonImg from "../assets/coin_exchange_images.png";
 import coinChart from "../assets/coin_charts_img.png";
 import BTCUSTD_Img from "../assets/BTCUSTD_Img.png";
+import dedicatedManagerImage from "../assets/dedicated_manager_image.png";
+import depositeBonusImage from "../assets/deposite_bonus_image.png";
+import iphone14blackMockup from "../assets/iPhone_14_Pro_Mockup.png";
+import iphone14blackMockup2 from "../assets/iPhone_14_Pro_Mockup2.png";
+import iphone14blackMockup3 from "../assets/iPhone_14_Pro_Mockup3.png";
+import userIconImage from "../assets/user_icon.png";
+import oneRupeeImage from "../assets/one_rupee.png";
+import arrowUpImage from "../assets/arrow_up.png";
+import backedByTheBestImage from "../assets/backed_by_the_best_image.png";
 export function Main() {
   return (
     <>
@@ -86,6 +104,7 @@ export function Main() {
           </h5>
         </div>
       </section>
+
       <section>
         <div className={overviewContainer}>
           <div>
@@ -118,7 +137,80 @@ export function Main() {
           </div>
         </div>
         <div className={BSTDIMG}>
-          <img src={BTCUSTD_Img}></img>
+          <img src={BTCUSTD_Img} alt="BTCUSTD"></img>
+        </div>
+      </section>
+      {/* start small earn big section */}
+      <section className={startSmallContainer}>
+        <div>
+          <h1>Start small. Earn Big.</h1>
+          <h5>
+            Deposity a minimum of 1000 and get a Deposit bonus+
+            <br />
+            dedicated relationship manager
+          </h5>
+        </div>
+        <div className={startSmallImagesContainer}>
+          <img src={depositeBonusImage} alt="deposite bonus image"></img>
+          <img src={dedicatedManagerImage} alt="dedicated manager image"></img>
+        </div>
+      </section>
+      {/* derivates section */}
+      <section className={derivatesContainer}>
+        <h1>
+          Derivates made simple <br />
+          in <b>3 Easy</b> steps.
+        </h1>
+        <div className={derivatesUserGuideContainer}>
+          <div className={guideBox}>
+            <div className={iphone14ImgContainer}>
+              <img src={iphone14blackMockup} />
+            </div>
+            <div className={guideInfoContainer}>
+              <img src={userIconImage} />
+              <h2>Create an Account</h2>
+              <h5>
+                Register and complete your <br />
+                Verification in less than 2 minutes
+              </h5>
+              <button>trade now</button>
+            </div>
+          </div>
+          <div className={guideBox}>
+            <div className={iphone14ImgContainer}>
+              <img src={iphone14blackMockup2} />
+            </div>
+            <div className={guideInfoContainer}>
+              <img src={oneRupeeImage} />
+              <h2>Deposit Funds</h2>
+              <h5>
+                Add funds quicklyusing a variety <br />
+                of payment methods
+              </h5>
+              <button>trade now</button>
+            </div>
+          </div>
+          <div className={guideBox}>
+            <div className={iphone14ImgContainer}>
+              <img src={iphone14blackMockup3} />
+            </div>
+            <div className={guideInfoContainer}>
+              <img src={arrowUpImage} />
+              <h2>Become a Trader</h2>
+              <h5>
+                Choose Your Trading Pair & Trade <br />
+                Seamlessly
+              </h5>
+              <button>trade now</button>
+            </div>
+          </div>
+        </div>
+        {/* backed by the best container */}
+        <div className={backedByTheBestContainer}>
+          <h1>Backed by the Best.</h1>
+          <div>
+            <img src={backedByTheBestImage} />
+          </div>
         </div>
       </section>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import * as React from "react";
+import '../styles/index.module.css';
+
+
 import { Navbar } from "../components/navbar";
 import { Header } from "../components/header";
 import { Main } from "../components/main";
+import { Footer } from "../components/footer";
 const IndexPage = () => {
   return (
     <>
       <Navbar />
       <Header />
-      <Main/>
+      <Main />
+      <Footer/>
     </>
   );
 };
